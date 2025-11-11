83d93625ce2c265b5f93675bc180ce022d8ff9f6
Oleksandr Hnutov <sanerhp.mail@gmail.com>
2025-11-11 22:47:10 +0200

    xyu

--- src/Page/Home.elm
+++ src/Page/Home.elm
@@ -5,4 +5,23 @@ import Html.Attributes exposing (..)
 
 view : Html msg
 view = 
-   a [ href "blog" ] [ text "Blog" ]
+   div
+   []
+   [ a [ href "blog" ] [ text "Blog" ]
+   , br [] []
+   , iframe 
+      [ width 110
+      , height 200
+      , src "https://www.myinstants.com/instant/khui-griu-8473/embed/"
+      , frameborder 0
+      , scrolling "no"
+      ] []
+   ]
+
+frameborder : Int -> Attribute msg
+frameborder n =
+  attribute "frameborder" (String.fromInt n)
+
+scrolling : String -> Attribute msg
+scrolling value =
+  attribute "scrolling" value
